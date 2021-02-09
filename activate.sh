887c2aa42339fe08655d3f03b6b6976ccf7dd473
David Ruhe <david.ruhe@gmail.com>
2021-02-09 15:19:20 +0100

    finished

--- activate.sh
+++ activate.sh
@@ -4,15 +4,18 @@ ENV_DIR='.venv'
 
 if [[ ! -d $ENV_DIR ]]
 then
-    echo "Installing required packages."
+    echo "Creating environment."
     python3 -m venv $ENV_DIR
-    source $ENV_DIR/bin/activate
-    pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
-else
-    echo "Activating environment."
-    source $ENV_DIR/bin/activate
 fi
 
+echo "Activating environment."
+source $ENV_DIR/bin/activate
+
+echo "Installing required packages."
+pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+
+source $ENV_DIR/bin/activate
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PYTHONPATH=$DIR
-export JUPYTER_PATH=$DIR
+export PYTHONPATH=$DIR/src
+export JUPYTER_PATH=$DIR/src
+cd $DIR/src
